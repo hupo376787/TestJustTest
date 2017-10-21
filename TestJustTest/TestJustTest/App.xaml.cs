d494d395a684cf64245bf060864ce3917b165a77
Vincent <376787823@qq.com>
2017-10-21 12:06:14 +0800

    Add files via upload

---
+++ TestJustTest/TestJustTest/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using TestJustTest.Services;
+
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+
+namespace TestJustTest
+{
+    public sealed partial class App : Application
+    {
+        private Lazy<ActivationService> _activationService;
+
+        private ActivationService ActivationService
+        {
+            get { return _activationService.Value; }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            // Deferred execution until used. Check https://msdn.microsoft.com/library/dd642331(v=vs.110).aspx for further info on Lazy<T> class.
+            _activationService = new Lazy<ActivationService>(CreateActivationService);
+        }
+
+        protected override async void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            if (!args.PrelaunchActivated)
+            {
+                await ActivationService.ActivateAsync(args);
+            }
+
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+        }
+
+        protected override async void OnActivated(IActivatedEventArgs args)
+        {
+            await ActivationService.ActivateAsync(args);
+        }
+
+        private ActivationService CreateActivationService()
+        {
+            return new ActivationService(this, typeof(Views.MainPage), new Views.ShellPage());
+        }
+    }
+}
---
+++ TestJustTest/TestJustTest/Views/CameraPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using TestJustTest.EventHandlers;
+
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace TestJustTest.Views
+{
+    public sealed partial class CameraPage : Page, INotifyPropertyChanged
+    {
+        public CameraPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CameraControl_PhotoTaken(object sender, CameraControlEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.Photo))
+            {
+                Photo.Source = new BitmapImage(new Uri(e.Photo));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
